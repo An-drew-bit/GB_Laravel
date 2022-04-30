e8fbe3407ff71c9a870f2b5da40fc4fbf8cbf81a
Andrew <mackarov.makoroff2016@yandex.ru>
2022-04-30 13:15:35 +0300

    commit3_3.2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
-use App\Http\Controllers\{HomeController};
+use App\Http\Controllers\{HomeController, UserController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)
     ->name('home');
+
+Route::get('/category', [])
+    ->name('category');
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/register', 'create')
+        ->name('register.create');
+
+    Route::post('/register', 'store')
+        ->name('register.store');
+
+    Route::get('/login', 'loginIn')
+        ->name('login.create');
+
+    Route::post('/login', 'login')
+        ->name('login');
+});
