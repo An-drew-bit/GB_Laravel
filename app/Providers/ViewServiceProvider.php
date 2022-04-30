2ec77a11438f03885da49c8185046d444f916591
Andrew <mackarov.makoroff2016@yandex.ru>
2022-04-30 14:04:29 +0300

    commit3_4

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,29 +2,19 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\{View, Auth};
+use App\View\Composers\AppComposer;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class ViewServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
-        Paginator::useBootstrap();
+        View::composer('*', AppComposer::class);
     }
 }
--- app/View/Composers/AppComposer.php
+++ app/View/Composers/AppComposer.php
@@ -2,7 +2,13 @@
 
 namespace App\View\Composers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
 class AppComposer
 {
-
+    public function compose(View $view, Auth $auth)
+    {
+        $view->with('name', $auth->user());
+    }
 }
