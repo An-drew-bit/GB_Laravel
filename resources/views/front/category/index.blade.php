098bf31344181d2939267835c3d359106eb96fc8
Andrew <mackarov.makoroff2016@yandex.ru>
2022-05-14 11:45:34 +0300

    commit4.1

--- resources/views/front/category/index.blade.php
+++ resources/views/front/category/index.blade.php
@@ -1,1 +1,19 @@
-<?php
+@extends('front.layouts.layout')
+
+@section('title')
+    <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="fw-light">Category page</h1>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('content')
+    <div class="album py-5 bg-light">
+        <div class="container">
+            asdasdasd
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,20 @@
 <?php
 
-use App\Http\Controllers\{HomeController, UserController};
+use App\Http\Controllers\{CategoryController, HomeController, UserController};
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class)
-    ->name('home');
+Route::get('/', HomeController::class)->name('home');
 
-Route::get('/category', [])
-    ->name('category');
+Route::controller(CategoryController::class)->group(function () {
+    Route::get('/category', 'index')->name('category');
+});
 
 Route::controller(UserController::class)->group(function () {
-    Route::get('/register', 'create')
-        ->name('register.create');
+    Route::get('/register', 'create')->name('register.create');
 
-    Route::post('/register', 'store')
-        ->name('register.store');
+    Route::post('/register', 'store')->name('register.store');
 
-    Route::get('/login', 'loginIn')
-        ->name('login.create');
+    Route::get('/login', 'loginIn')->name('login.create');
 
-    Route::post('/login', 'login')
-        ->name('login');
+    Route::post('/login', 'login')->name('login');
 });
