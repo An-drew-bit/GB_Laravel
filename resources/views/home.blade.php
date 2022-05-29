41dd0f79f06892696ed3dc73edbcb7982654741b
Andrew <mackarov.makoroff2016@yandex.ru>
2022-05-29 22:16:31 +0300

    commit_7.1.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,7 @@
                                  xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
                                  preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
                                 <rect width="100%" height="100%" fill="#55595c"/>
-                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                                <text x="40%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
                             </svg>
 
                             <div class="card-body">
